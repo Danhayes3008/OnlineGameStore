1970dc5aa088e8dea4e3cfba5b958c6523ff68d3
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2022-04-24 18:12:34 +0100

    create basic product model
    created the product model to contain the product information, created the device model as the games will not all be using same device to run and some games will have multiple devices that they can be bought for.

--- Inventory/models.py
+++ Inventory/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class  Device(models.Model):
+    name = models.CharField(max_length=250)
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, null=True, blank=False)
+    device = models.OneToOneField(Device, on_delete=models.CASCADE, related_name='Device', null=True, Blank=False)
+    price = models.DecimalField(decimal_places=2, max_digits=7, null=True, blank=False)
+    decription = models.CharField(max_length=500, null=True, blank=False)
+    releaseDate = models.DateField(auto_now=False, null=True, Blank=True)
+    
