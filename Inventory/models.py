ab5e52d257966b410a5e6fda3e1fbade98d72198
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2022-04-26 17:58:52 +0100

    fixed typo

--- Inventory/models.py
+++ Inventory/models.py
@@ -5,8 +5,8 @@ class  Device(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=250, null=True, blank=False)
-    device = models.OneToOneField(Device, on_delete=models.CASCADE, related_name='Device', null=True, Blank=False)
+    device = models.OneToOneField(Device, on_delete=models.CASCADE, related_name='Device', null=True, blank=False)
     price = models.DecimalField(decimal_places=2, max_digits=7, null=True, blank=False)
     decription = models.CharField(max_length=500, null=True, blank=False)
-    releaseDate = models.DateField(auto_now=False, null=True, Blank=True)
+    releaseDate = models.DateField(auto_now=False, null=True, blank=True)
     
